cd30f8d3ae5cde8bc266b47acfbc2d5bcab209c9
Leonardo Negrão <leonardolnegrao@gmail.com>
2020-10-07 20:26:57 -0300

    creates project

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("Bem vindo ao Bytebank!")
+    val titular: String = "Leonardo"
+    val conta: String = "55453-4"
+    val agencia: Int = 1000
+    var saldo: Double = 0.0
+
+    saldo = 100.0
+
+    println("Titular: $titular, da Agência $agencia, com conta número $conta e saldo de $saldo")
+}
