2cfecd6061078d4d18d06bb56959d33af44b2c58
Leonardo Negrão <leonardolnegrao@gmail.com>
2020-10-08 18:22:09 -0300

    creates classes Account and AccountHelpers

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,72 @@
 fun main() {
-    println("Bem vindo ao Bytebank!")
-    val titular: String = "Leonardo"
-    val conta: String = "55453-4"
-    val agencia: Int = 1000
-    var saldo: Double = 0.0
+    println("Welcome to Bytebank!")
 
-    saldo = 100.0
+    val account = Account("Leonardo Negrão", "0001")
+    account.deposit(0.0)
+    val balance = account.getBalance()
+    println("Your current balance is $balance")
+}
 
-    println("Titular: $titular, da Agência $agencia, com conta número $conta e saldo de $saldo")
+class Account(var owner: String, var accountNumber: String) {
+    private val agency: Int = 1000
+    private var balance: Double = 0.0
+    
+    fun getAgency(): Int {
+        return this.agency
+    }
+
+    fun getBalance(): Double {
+        return this.balance
+    }
+
+    fun setBalance(value: Double) {
+        if (value <= 0) {
+            this.balance -= value
+        } else {
+            this.balance += value
+        }
+    }
+
+    fun deposit(value: Double) {
+        val depositIsValid = AccountHelpers.validateDeposit(value)
+
+        if (depositIsValid) {
+            this.balance += value
+        } else {
+            throw Exception("Deposit value is invalid!")
+        }
+    }
+
+    fun withdraw(value: Double) {
+        val withdrawIsValid = AccountHelpers.validateWithdraw(value, this.balance)
+
+        if (withdrawIsValid) {
+            this.balance -= value
+        } else {
+            throw Exception("Withdraw value is invalid!")
+        }
+    }
+
+    fun wireTransfer(receiver: Account, value: Double) {
+        val wireTransferIsValid = AccountHelpers.validateWithdraw(value, this.balance)
+
+        if (wireTransferIsValid) {
+            this.balance -= value
+            receiver.deposit(value)
+        }
+    }
+}
+
+class AccountHelpers() {
+    companion object {
+        fun validateDeposit(value: Double): Boolean {
+            if (value < 0) return false
+            return true
+        }
+
+        fun validateWithdraw(value: Double, currentBalance: Double): Boolean {
+            if (value > currentBalance) return false
+            return true
+        }
+    }
 }
